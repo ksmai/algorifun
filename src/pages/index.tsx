a6463f56e1853d123d5728641f84ba715405b977
ksmai <kasingmai@gmail.com>
2019-12-15 11:09:18 +0800

    Integrate typography.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+  import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 
 import Layout from '../components/layout';
@@ -7,19 +8,33 @@ import Image from '../components/image';
 import SEO from '../components/seo';
 
 const Headline = styled.h1`
-  font-size: 24px;
   color: red;
   text-decoration: underline;
 `;
 
 const greet: string = 'Test Again';
 
+const myColor = '#ff00ff';
+const anotherColor = '#00ff00'
+const goodCSS = css`
+  color: ${myColor};
+`;
+const anotherGoodCSS = css`
+  ${goodCSS};
+  background-color: ${anotherColor};
+  &::before {
+    display: block;
+    content: "ABC";
+    background-color: black;
+  }
+`;
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Headline>{greet}</Headline>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p css={goodCSS}>Welcome to your new Gatsby site.</p>
+    <p css={anotherGoodCSS}>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import funstonTheme from 'typography-theme-funston'
+
+const typography = new Typography(funstonTheme)
+console.log(typography, funstonTheme)
+
+export default typography
