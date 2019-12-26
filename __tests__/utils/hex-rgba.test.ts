87929d64708bceb32039d49b6ee045d256a45e2a
ksmai <kasingmai@gmail.com>
2019-12-26 14:07:21 +0800

    Fix alpha range in hexRGBA

--- __tests__/utils/hex-rgba.test.ts
+++ __tests__/utils/hex-rgba.test.ts
@@ -4,10 +4,10 @@ type Case = [string, number, string, string];
 
 describe('hexRGBA', () => {
     const successCases: Case[] = [
-        ['#abcdef', 42, 'rgba(171,205,239,42)', 'converts 6 digits with #'],
-        ['42390f', 255, 'rgba(66,57,15,255)', 'converts 6 digits without #'],
-        ['#f0c', 0, 'rgba(255,0,204,0)', 'converts 3 digits with #'],
-        ['123', 1, 'rgba(17,34,51,1)', 'converts 3 digits without #'],
+        ['#abcdef', 1, 'rgba(171,205,239,1)', 'converts 6 digits with #'],
+        ['42390f', 0.9, 'rgba(66,57,15,0.9)', 'converts 6 digits without #'],
+        ['#f0c', 0.3, 'rgba(255,0,204,0.3)', 'converts 3 digits with #'],
+        ['123', 0, 'rgba(17,34,51,0)', 'converts 3 digits without #'],
     ]
     successCases.forEach(([hex, alpha, expected, description]) => {
         it(description, () => {
@@ -17,14 +17,14 @@ describe('hexRGBA', () => {
     });
 
     const failureCases: Case[] = [
-        ['zzz', 42, '', 'throws when hex digits are out of range'],
-        ['#abx', 42, '', 'throws when hex digits are out of range'],
-        ['12p34d', 42, '', 'throws when hex digits are out of range'],
-        ['#12p34d', 42, '', 'throws when hex digits are out of range'],
-        ['#fff', 256, '', 'throws when alpha is out of range'],
+        ['zzz', 1, '', 'throws when hex digits are out of range'],
+        ['#abx', 1, '', 'throws when hex digits are out of range'],
+        ['12p34d', 1, '', 'throws when hex digits are out of range'],
+        ['#12p34d', 1, '', 'throws when hex digits are out of range'],
+        ['#fff', 2, '', 'throws when alpha is out of range'],
         ['#fff', -1, '', 'throws when alpha is out of range'],
-        ['#aaaaaaa', 42, '', 'throws when wrong number of digits'],
-        ['#aaaa', 42, '', 'throws when wrong number of digits'],
+        ['#aaaaaaa', 1, '', 'throws when wrong number of digits'],
+        ['#aaaa', 1, '', 'throws when wrong number of digits'],
     ];
     failureCases.forEach(([hex, alpha, expected, description]) => {
         it(description, () => {
--- src/utils/hex-rgba.ts
+++ src/utils/hex-rgba.ts
@@ -1,13 +1,13 @@
 /**
  * @param hex the css hex color, e.g. #FFF, #abcdef, ff0000
- * @param alpha 0-255
+ * @param alpha 0-1
  * @return the css rgba string
  */
 const RE = /^#?([0-9A-F]{3}|[0-9A-F]{6})$/i;
 
-export default function hexRGBA(hex: string, alpha = 255) {
-    if (alpha < 0 || alpha > 255) {
-        throw new Error(`Expected alpha to be between 0 and 255, got: ${alpha}`);
+export default function hexRGBA(hex: string, alpha = 1) {
+    if (alpha < 0 || alpha > 1) {
+        throw new Error(`Expected alpha to be between 0 and 1, got: ${alpha}`);
     }
     const matches = hex.match(RE);
     if (!matches) {
